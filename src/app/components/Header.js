dfe0a5bcecba6c6664070fd88dfd6ccbe63c608b
maiaramorit <maiaramorit@gmail.com>
2023-07-12 14:34:43 -0300

    darkMode

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,52 +1,55 @@
-import {HomeIcon, MoonIcon} from "@heroicons/react/solid";
+import { HomeIcon, MoonIcon, SunIcon } from "@heroicons/react/solid";
 
-export const Header = () => {
+export const Header = ({ darkMode, toggleTheme }) => {
   const arrayBtnsLeft = [
-    <HomeIcon className="w-6 h-6"/>,
-    <MoonIcon className="w-6 h-6"/>
-  ]
-  const arrayBtnsRight = [
-    'Projetos',
-    'Formação',
-    'Experiências'
-  ]
-  const navBarDivsClassName = 'lg:flex lg:flex-1'
+    <HomeIcon className="w-6 h-6" />,
+    darkMode ? <SunIcon className="w-6 h-6" /> : <MoonIcon className="w-6 h-6" />,
+  ];
+
+  const arrayBtnsRight = ["Projetos", "Formação", "Experiências"];
+
+  const navBarDivsClassName = "lg:flex lg:flex-1";
+
   return (
     <div>
-      <div>
-        <header id='header' className="bg-orange shadow-md pb-2">
-          <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-            <div className={`${navBarDivsClassName} lg:justify-start`}>
-              {
-                arrayBtnsLeft.map((botao, index) => {
-                  return (
-                    <div key={`header-btns-left-${index}`} className="cursor-pointer inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                         aria-controls="mobile-menu"
-                         aria-expanded="false"
-                    >
-                      {botao}
-                    </div>
-                  )
-                })
-              }
-            </div>
-            <div className={`${navBarDivsClassName} lg:justify-end`}>
-              {
-                arrayBtnsRight.map((botao, index) => {
-                  return (
-                    <div key={`header-btns-right-${index}`} className="cursor-pointer inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                         aria-controls="mobile-menu"
-                         aria-expanded="false"
-                    >
-                      {botao}
-                    </div>
-                  )
-                })
-              }
-            </div>
-          </nav>
-        </header>
-      </div>
+      <header
+        id="header"
+        className={`bg-orange shadow-md pb-2 ${darkMode ? "dark bg-gray-800" : ""}`}
+      >
+        <nav
+          className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+          aria-label="Global"
+        >
+          <div className={`${navBarDivsClassName} lg:justify-start`}>
+            {arrayBtnsLeft.map((botao, index) => (
+              <div
+                key={`header-btns-left-${index}`}
+                className={`cursor-pointer inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white ${
+                  darkMode ? "dark:text-white" : ""
+                }`}
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+                onClick={toggleTheme}
+              >
+                {botao}
+              </div>
+            ))}
+          </div>
+          <div className={`${navBarDivsClassName} lg:justify-end`}>
+            {arrayBtnsRight.map((botao, index) => (
+              <div
+                key={`header-btns-right-${index}`}
+                className="cursor-pointer inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+              >
+                {botao}
+              </div>
+            ))}
+          </div>
+        </nav>
+      </header>
     </div>
-  )
-}
+  );
+};
+
--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,19 @@
 "use client";
 
-import {Header} from "@/app/components/Header";
-import {useState} from "react";
-export default function Home() {
-  const [inputText, setInputText] = useState('')
+import { Header } from "@/app/components/Header";
+import { useState } from "react";
 
-  const handleInputChange = (e) => {
-    // console.log(e.target.value)
-    setInputText(e.target.value)
-  }
+export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setDarkMode(!darkMode);
+  };
 
   return (
-    <main>
-        <Header />
-      {/* <input type={'text'} value={inputText} onChange={e => handleInputChange(e)}/> */}
-    </main>
-  )
+<main className={darkMode ? "dark" : ""}>
+  <Header darkMode={darkMode} toggleTheme={toggleTheme} />
+  <div className="flex flex-grow min-h-screen mt-0 dark:bg-gray-800"/>
+</main>
+  );
 }
