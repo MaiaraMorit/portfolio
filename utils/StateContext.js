eebac7892c0e751f3640f9000f9219b4892e1bac
maiaramorit <maiaramorit@gmail.com>
2023-07-16 08:35:58 -0300

    fix: add missing file

---
+++ utils/StateContext.js
@@ -0,0 +1,26 @@
+"use client"
+
+import React, { useState } from 'react';
+
+const StateContext = React.createContext();
+
+export const StateProvider = ({ children }) => {
+  const [sharedState, setSharedState] = useState({
+    theme: 'dark',
+  });
+
+  const updateSharedState = (newState) => {
+    setSharedState((prevState) => ({
+      ...prevState,
+      ...newState,
+    }));
+  };
+
+  return (
+    <StateContext.Provider value={{ sharedState, updateSharedState }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export default StateContext;
