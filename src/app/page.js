b7ec8fa012d18a7959693466803f19b2a292ecde
MaiaraMorit <maiaramorit@gmail.com>
2023-08-07 22:35:25 -0300

    refacture: minhas habilidades

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
 import { Header } from "@/app/components/Header";
 import { Sobre } from "@/app/components/Sobre";
-// import { Projetos } from "@/app/components/Projetos";
 import MainContainer from "@/app/components/MainContainer";
 
 export default function Home() {
   return (
-    <div style={{ overflowY: 'scroll', maxHeight: '95vh' }}>
+    <div style={{ overflowY: 'scroll', maxHeight: '100vh' }}>
        <MainContainer>
       <Header />
       <Sobre />
